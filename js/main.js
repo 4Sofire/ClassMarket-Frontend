f5c6a71bb33c0a5d716fd0070a759466a0081a51
4Sofire <137798321+4Sofire@users.noreply.github.com>
2025-12-05 22:13:58 +0400

    Name Sort Complete

--- js/main.js
+++ js/main.js
@@ -6,12 +6,13 @@ let app = new Vue(
         lastname: 'Walker',
         courses: [],
         cart: [],
+        sortAttribute: 'name',
+        sortOrder: 'asc',
     }, 
     methods: {
         addToCart(course){
-
             if(course.availableSpaces>0){
-                let existingCartItem = this.cart.find(item => item._id --- course.id);
+                let existingCartItem = this.cart.find(item => item._id === course.id);
 
                 if (existingCartItem){
                     existingCartItem.quantity +=1;  
@@ -35,6 +36,15 @@ let app = new Vue(
         goToCart(course){
             window.location.href = "./cart.html";
         },
+        sort(){
+            console.log(`Sorting by ${this.sortAttribute} in ${this.sortOrder}`);
+            if(this.sortAttribute === 'name' && this.sortOrder === 'asc'){
+                this.courses.sort(function(a, b) { return a.name.localeCompare(b.name)})
+            }
+            else if(this.sortAttribute === 'name' && this.sortOrder === 'desc'){
+                this.courses.sort(function(a, b) { return a.name.localeCompare(b.name)}).reverse()
+            }
+        }
     },
     computed: {
         fullname: function(){
